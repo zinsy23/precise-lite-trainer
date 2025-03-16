76ca935ff533bdc36f9e08f79dbf95172d40eeb6
zinsy23 <jdzinsy@gmail.com>
2025-03-15 21:11:27 -0500

    added runtime script

---
+++ training.py
@@ -0,0 +1,51 @@
+from precise_trainer import PreciseTrainer
+
+model_name = "computer"
+folder = f"computer/{model_name}"  # dataset here
+model_path = f"computer/model/{model_name}"  # save here
+log_dir = f"logs/fit/{model_name}"  # for tensorboard
+
+# train a model
+trainer = PreciseTrainer(model_path, folder, epochs=100, log_dir=log_dir)
+model_file = trainer.train()
+# Data: <TrainData wake_words=155 not_wake_words=89356 test_wake_words=39 test_not_wake_words=22339>
+# Loading wake-word...
+# Loading not-wake-word...
+# Loading wake-word...
+# Loading not-wake-word...
+# Inputs shape: (81602, 29, 13)
+# Outputs shape: (81602, 1)
+# Test inputs shape: (20486, 29, 13)
+# Test outputs shape: (20486, 1)
+# Model: "sequential"
+# _________________________________________________________________
+#  Layer (type)                Output Shape              Param #   
+# =================================================================
+#  net (GRU)                   (None, 20)                2100      
+#                                                                  
+#  dense (Dense)               (None, 1)                 21        
+#                                                                  
+# =================================================================
+# Total params: 2,121
+# Trainable params: 2,121
+# Non-trainable params: 0
+# .....
+# _________________________________________________________________
+# Epoch 1280/1379
+# 157/160 [============================>.] - ETA: 0s - loss: 0.0308 - accuracy: 0.9868
+# ....
+# Wrote to /home/miro/PycharmProjects/ovos-audio-classifiers/trained/hey_computer/model.tflite
+trainer.test()
+
+# === Counts ===
+# False Positives: 2
+# True Negatives: 20445
+# False Negatives: 2
+# True Positives: 37
+# 
+# === Summary ===
+# 20482 out of 20486
+# 99.98%
+# 
+# 0.01% false positives
+# 5.13% false negatives
